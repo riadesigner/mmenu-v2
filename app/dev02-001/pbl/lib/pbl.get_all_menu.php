b6ef237c431bdfdbcbf283d8ca534319ba26dac3
riadesigner <evgenii.pogrebnyak@gmail.com>
2024-04-04 11:36:54 +1000

    ok

--- app/dev02-001/pbl/lib/pbl.get_all_menu.php
+++ app/dev02-001/pbl/lib/pbl.get_all_menu.php
@@ -8,14 +8,13 @@
 	$callback = $_REQUEST['callback'] ?? 'alert';
 	if (!preg_match('/^[a-z0-9_-]+$/i', (string) $callback)) {  $callback = 'alert'; }
 
-	
-	define("BASEPATH",__file__);
-	
+	define("BASEPATH",__file__);		
+
 	require_once '../../../config.php';
 	require_once '../../../vendor/autoload.php';
 	
-	require_once '../../core/common.php';
-	
+	require_once '../../core/common.php';	
+
 	require_once '../../core/class.sql.php';
 	 
 	require_once '../../core/class.smart_object.php';
