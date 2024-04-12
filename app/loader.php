4d36c543e810a1d930eb124de494d350ea7c6f69
riadesigner <evgenii.pogrebnyak@gmail.com>
2024-04-12 22:43:15 +1000

    ok

--- app/loader.php
+++ app/loader.php
@@ -5,21 +5,19 @@ require_once 'config.php';
 
 header('Content-Type: text/javascript');
 
-echo "console.log('test 555');";
+$app_server = $CFG->base_url;
+$app_base = $CFG->base_app_url;
+$app_version = $CFG->version;
 
-// $app_server = $CFG->base_url;
-// $app_base = $CFG->base_app_url;
-// $app_version = $CFG->version;
+$filename = "loader-tpl.js";
+$file=fopen($filename,"r");
+$loader = fread($file,filesize($filename));
+fclose($file);
 
-// $filename = "loader-tpl.js";
-// $file=fopen($filename,"r");
-// $loader = fread($file,filesize($filename));
-// fclose($file);
+$loader = str_replace("%[app_server]%",$app_server,$loader);
+$loader = str_replace("%[app_base]%",$app_base,$loader);
+$loader = str_replace("%[app_version]%",$app_version,$loader);
 
-// $loader = str_replace("%[app_server]%",$app_server,$loader);
-// $loader = str_replace("%[app_base]%",$app_base,$loader);
-// $loader = str_replace("%[app_version]%",$app_version,$loader);
-
-// echo $loader;
+echo $loader;
 
 ?>
