0e6457ebe8ccda9b683c9b7df8552896da1a882c
riadesigner <evgenii.pogrebnyak@gmail.com>
2024-07-23 16:08:12 +1000

    ok

--- app/iikohook.php
+++ app/iikohook.php
@@ -2,13 +2,11 @@
 
 define("BASEPATH",__file__);
 require_once 'config.php';
+require_once WORK_DIR.APP_DIR.'core/common.php';
 
-
-$filename = "iiko_log";
 $data = json_decode(file_get_contents('php://input'), TRUE);
 
-
-glog("iikohook answer: ".print_r($data, 1),$filename);
+glog("iikohook answer: ".print_r($data, 1),__FILE__);
 
 
 
--- app/tg-hook-dev.php
+++ app/tg-hook-dev.php
@@ -1,6 +1,11 @@
 <?php
 
-$filename = "tg_hook_dev_deta";
+define("BASEPATH",__file__);
+require_once 'config.php';
+require_once WORK_DIR.APP_DIR.'core/common.php';
+
 $data = json_decode(file_get_contents('php://input'), TRUE);
 
-glog("tg cart dev bot answer: ".print_r($data, 1),$filename);
+glog("tg cart dev bot answer: ".print_r($data, 1),__FILE__);
+
+?>
