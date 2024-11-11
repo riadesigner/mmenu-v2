4d00a51c691edbec76fe11736376d878e2ec0b85
riadesigner <evgenii.pogrebnyak@gmail.com>
2024-11-11 18:43:00 +1000

    ok

---
+++ app/export/app-v2-056/adm/lib/iiko/iiko.get_modif_dictionary.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+	Add menu to cafe
+
+*/	
+	header('content-type: application/json; charset=utf-8');
+	$callback = $_REQUEST['callback'] ?? 'alert';
+	if (!preg_match('/^[a-z0-9_-]+$/i', (string) $callback)) {  $callback = 'alert'; }	
+
+	define("BASEPATH",__file__);
+	
+	require_once getenv('WORKDIR').'/config.php';
+
+	require_once WORK_DIR.APP_DIR.'core/common.php';	
+	
+	require_once WORK_DIR.APP_DIR.'core/class.sql.php';
+	 
+	require_once WORK_DIR.APP_DIR.'core/class.smart_object.php';
+	require_once WORK_DIR.APP_DIR.'core/class.smart_collect.php';
+	require_once WORK_DIR.APP_DIR.'core/class.user.php';
+	
+	require_once WORK_DIR.APP_DIR.'core/class.account.php';
+	require_once WORK_DIR.APP_DIR.'core/class.tg_keys.php';	
+	require_once WORK_DIR.APP_DIR.'core/class.email.php';		
+
+	session_start();
+	SQL::connect();
+
+	$user = User::from_cookie();
+	if(!$user || !$user->valid())__errorjsonp("Unknown user, ".__LINE__);
+	
+	if(!isset($_POST['id_cafe']) || empty($_POST['id_cafe']) ) __errorjsonp("Unknown id cafe, ".__LINE__);
+	$id_cafe = (int) $_POST['id_cafe'];
+	$cafe = new Smart_object("cafe",$id_cafe);
+	if(!$cafe->valid())__errorjsonp("Unknown cafe, ".__LINE__);	
+
+	if($cafe->id_user !== $user->id) __errorjsonp("Not allowed, ".__LINE__);
+
+	$modif_dict = new Smart_collect("iiko_params", "where id_cafe={$id_cafe}");
+	if($modif_dict->found()){
+		$arr = json_decode((string) $modif_dict->get(),1); 
+	}else{
+		$arr = [];
+	}
+	__answerjsonp($arr);
+
+
+?>
