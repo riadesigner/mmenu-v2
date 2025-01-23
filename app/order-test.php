eb428e43496c18454629f12348e399b14b24216e
riadesigner <evgenii.pogrebnyak@gmail.com>
2025-01-23 15:41:13 +1000

    ok

--- app/order-test.php
+++ app/order-test.php
@@ -2,12 +2,36 @@
 
 define("BASEPATH",__file__);
 require_once 'config.php';
-
+// require_once getenv('WORKDIR').'/config.php';
 require_once WORK_DIR.APP_DIR.'core/class.sql.php';
 require_once WORK_DIR.APP_DIR.'core/common.php';
-glog("test crontab");
+require_once WORK_DIR.APP_DIR.'core/class.sql.php';
+require_once WORK_DIR.APP_DIR.'core/class.smart_object.php';
+require_once WORK_DIR.APP_DIR.'core/class.smart_collect.php';
+require_once WORK_DIR.APP_DIR.'core/class.user.php';
+
+SQL::connect();
+
+$orders = new Smart_collect("orders", "WHERE state = 'created'",  "ORDER BY 'cafe_uniq_name'");
 
 $time = date('Y-m-d H:i:s');
-echo "CHECK ORDERS! $time\n";
+
+if($orders && $orders->full()){
+    glog("$time check orders: ".$orders->found());
+}else{
+    glog("$time check orders: ok");
+}
+
+echo "$time check orders\n";
+
+
+
+
+
+
+
+
+
+
 
 ?>
