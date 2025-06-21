80fdb2d4c89dc2dfeaafc4263e75fbaab8a726b5
riadesigner <evgenii.pogrebnyak@gmail.com>
2025-06-21 18:36:29 +1000

    Временная отладка

---
+++ app/export/app-v2-114/adm/lib/iiko/get_oldway_menu_by_id.php
@@ -0,0 +1,127 @@
+<?php
+
+header('content-type: application/json; charset=utf-8');
+$callback = $_REQUEST['callback'] ?? 'alert';
+if (!preg_match('/^[a-z0-9_-]+$/i', (string) $callback)) {  $callback = 'alert'; }   
+
+define("BASEPATH",__file__);
+
+require_once getenv('WORKDIR').'/config.php';
+
+require_once WORK_DIR.APP_DIR.'core/common.php';    
+
+require_once WORK_DIR.APP_DIR.'core/class.sql.php';
+ 
+require_once WORK_DIR.APP_DIR.'core/class.smart_object.php';
+require_once WORK_DIR.APP_DIR.'core/class.smart_collect.php';
+require_once WORK_DIR.APP_DIR.'core/class.user.php';
+require_once WORK_DIR.APP_DIR.'core/class.iiko_params.php';
+require_once WORK_DIR.APP_DIR.('core/class.iiko_nomenclature.php');
+require_once WORK_DIR.APP_DIR.('core/class.iiko_parser_to_unimenu.php');
+require_once WORK_DIR.APP_DIR.('core/class.conv_unimenu_to_chefs.php');
+
+session_start();
+SQL::connect();
+
+
+// GETTING OLDWAY MENU BY ID / API 1, (FROM NOMENCLATURE) 
+
+if(!isset($_POST['token'])){
+	__errorjsonp(["error"=>"unknown token"]);
+	exit();
+}
+if(!isset($_POST['externalMenuId'])){
+    __errorjsonp(["error"=>"unknown external menu id"]);
+    exit();
+}
+if(!isset($_POST['id_cafe']) || empty($_POST['id_cafe']) ){
+    __errorjsonp(["error"=>"--its need to know id_cafe"]);
+    exit();
+}
+
+$token = $_POST['token'];
+$externalMenuId = $_POST['externalMenuId'];
+// $currentExtmenuHash = $_POST['currentExtmenuHash'];
+$id_cafe = post_clean($_POST['id_cafe']);
+
+$id_cafe = (int) $_POST['id_cafe'];
+$cafe = new Smart_object("cafe",$id_cafe);
+if(!$cafe->valid())__errorjsonp("Unknown cafe, ".__LINE__);
+
+// $iiko_params_collect = new Smart_collect("iiko_params", "where id_cafe='".$cafe->id."'");
+// if(!$iiko_params_collect || !$iiko_params_collect->full()) __errorjsonp("--cant find iiko params for cafe ".$cafe->id);
+// $iiko_params = $iiko_params_collect->get(0);
+// $organization_id = $iiko_params->current_organization_id;
+$key = $cafe->iiko_api_key;
+$IIKO_PARAMS = new Iiko_params($id_cafe, $key);
+$orgId = ($IIKO_PARAMS->get())->current_organization_id;
+
+// -------------------------------------------------------
+// загружаем номенклатуру из тестового файла
+// (реальный ответ от iiko, API 1, получение номенклатуры)
+// -------------------------------------------------------
+// $file_path = WORK_DIR.'/files/json-info-formated-full-new.json';
+// $iiko_response = get_response_from_test_file($file_path);
+// $menu_id = "9da77ff8-862d-45e4-a7f2-a5117910fa66";
+
+// -------------------------------------------------------
+// получаем номенклатуру с сервера iiko
+// -------------------------------------------------------
+$NOMCL = new Iiko_nomenclature($orgId, "", $token);    
+$NOMCL->reload();
+$iiko_response = $NOMCL->get_data();
+
+glog("IIKO_RESPONSE  ========== ".print_r($iiko_response,1));
+
+
+// -------------------------------------------------------
+// используем папки как категории (false, если PIZZAIOLO)
+// -------------------------------------------------------
+define("GROUPS_AS_CATEGORIES", ($IIKO_PARAMS->get())->current_nomenclature_type=='groups_as_categories'); 
+
+// преобразуем ее в формат UNIMENU
+$UNIMENU = new iiko_parser_to_unimenu("", $iiko_response);
+$UNIMENU->parse(GROUPS_AS_CATEGORIES); 
+$data = $UNIMENU->get_data();
+
+glog("UNIMENU  ========== ".print_r($data,1));
+
+// конвертим ее в текущий формат CHEFSMENU
+$CHEFS_CONVERTER = new Conv_unimenu_to_chefs($data);
+$chefsdata = $CHEFS_CONVERTER->convert()->get_data();
+
+$menu = $chefsdata["Menus"][$externalMenuId]??null;
+
+glog("CHEFSDATA ========== ".print_r($chefsdata,1));
+
+// $res = iiko_get_info($url,$headers,$params);
+// $newExtmenuHash = md5(json_encode($res, JSON_UNESCAPED_UNICODE));
+// $need2update = $currentExtmenuHash!==$newExtmenuHash;
+
+$answer = [
+        "menu"=>$menu,
+        "menu-hash"=>"1",
+        "need-to-update"=>true
+    ];
+
+__answerjsonp($answer);
+
+
+
+function get_response_from_test_file($file_path): array{
+    // Проверяем существование файла
+    if (!file_exists($file_path)) {
+        __errorjsonp("Ошибка: Файл не найден.");
+    }
+    // Читаем содержимое файла
+    $json_data = file_get_contents($file_path);
+    if ($json_data === false) {
+        __errorjsonp("Ошибка: Не удалось прочитать файл.");
+    }
+    // Декодируем JSON в ассоциативный массив
+    $iiko_response = json_decode($json_data, true);
+
+    return $iiko_response;
+}
+
+?>
---
+++ app/export/app-v2-114/core/class.conv_unimenu_to_chefs.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * ПРЕОБРАЗУЕМ ФОРМАТ UNIMENU В CHEFSMENU (v-2.3.1)
+ * 
+ * для виртуального размерного ряда:
+ * - добавлено поле originalPrice (из modifier->sizes[0]->price) 
+ * - добавлено поле sizeGroupId (из modifierGroupId)
+ * (для возможности перед отправкой заказа в iiko  
+ * распарсить размерный ряд обратно в модификатор размера, 
+ * из которого он был собран)
+ * 
+ * - добавлены названия для размерного ряда, если он есть, а имена пустые
+ * (такое может быть, если используется настоящий размерный ряд, 
+ * но загружается меню как Номенклатура, а не как Внешнее меню)
+ * 
+ * особенность формата chefsmenu в том (в том числе), что 
+ * CATEGORIES и ITEMS (ТОВАРЫ) - хранятся как ассоциативный массив (с id ключами ),
+ * а MODIFIERS и ITEMS (ГРУППЫ МОДИФИКАТОРОВ и МОДИФИКАТОРЫ) - как обычные массивы (с индексами 0, 1, 2, ...);
+ * 
+ * */
+
+class Conv_unimenu_to_chefs {
+    
+    private array $UNIMENU;    
+    private array $DATA;    
+
+
+	function __construct(array $unimenu){				
+        $this->UNIMENU = $unimenu;
+		return $this;
+	}
+
+    public function convert(): Conv_unimenu_to_chefs {
+        $ready_menus = array(); 
+
+        $menus = $this->UNIMENU['Menus'];
+
+        foreach($menus as $menu){
+            $ready_menus[$menu["menuId"]] = $this->convert_menu($menu);
+        }
+
+        $this->DATA = $ready_menus;
+        return $this;
+    }
+
+    public function get_data(): array {
+        return [
+            "SourceMenus" => "NOMENCLATURE",
+            "TypeMenus" => "CHEFSMENU",
+            "TotalMenus" => count($this->DATA),
+            "Menus" => $this->DATA,            
+        ];        
+    }
+
+    private function convert_menu(array $menu): array {
+        $data = array();
+        $data['id'] = $menu['menuId'];
+        $data['name'] = $menu['name'];
+        $data['description'] = $menu['description'];
+        $data['categories'] = $this->get_categoties($menu['groups']);        
+        $counter = 0;
+        foreach($data['categories'] as &$category){
+            $counter++;
+            $category['items'] = $this->get_items($menu, $category["id"]);
+        }
+        return $data;
+    }
+
+    // собираем категории
+    private function get_categoties(array $groups): array {        
+        $categories = array_filter($groups, fn($e) => $e["type"] === "CATEGORY");
+        $categories = array_map(fn($e) => [
+            "id" => $e["groupId"],
+            "name" => $e["name"],
+        ], $categories);
+        return $categories;
+    }
+
+    // собираем товары
+    private function get_items(array $menu, string $category_id): array {
+       
+        $items = array_filter($menu['products'], fn($e) => $e["parentGroup"] === $category_id);
+        
+        $items_parsed = array_map(function($e) use($menu) { 
+                        
+            [$gNormalModifiers, $gSizesModifiers] = $this->get_modifiers_goups($menu, $e["groupModifiers"]);
+
+            $gNormalModifiers = $this->assoc_to_array($gNormalModifiers);
+            
+            $sizes = $this->get_item_sizes($e["itemSizes"], $gSizesModifiers);
+
+            return [
+            "id" => $e["itemId"],
+            "name" => $e["name"],
+            "description" => $e["description"],
+            "imageUrl"=> $e["imageUrl"],
+            "sizes"=> $sizes,
+            "modifiers"=> $gNormalModifiers,
+            "orderItemType"=>"",
+            "sku"=>"",        
+        ];}, $items);
+        return $items_parsed;
+    }
+
+    // собираем группы модификаторов
+    private function get_modifiers_goups(array $menu, array $groupModifiers): array {        
+
+        $gModifiers = array_map(fn($e) => [
+            "modifierGroupId" => $e["modifierGroupId"],            
+            "name" =>  $menu["groups"][$e["modifierGroupId"]]["name"]??"",
+            "restrictions"=>[
+                "minQuantity"=> $e["restrictions"]["minQuantity"],
+                "maxQuantity"=> $e["restrictions"]["maxQuantity"],
+                "required"=>$e["restrictions"]["required"],
+                "freeQuantity"=> $e["restrictions"]["freeQuantity"],
+                "byDefault"=> $e["restrictions"]["byDefault"],                
+            ],
+            "items"=> $this->get_modifiers_items($menu, $e["modifierGroupId"], $e["childModifiers"]),
+            ],$groupModifiers);
+
+        // ФИШКА PIZZAIOLO:
+        // делим группы модификаторов
+        // на ОБЫЧНЫЕ и на МОДИФИКАТОРЫ РАЗМЕРА
+        // (если в названии группы модификаторов есть слово "размер")
+        $gNormalModifiers = array_filter($gModifiers, fn($e) => !str_contains(mb_strtolower($e["name"]), "размер"));
+        $gSizesModifiers = array_filter($gModifiers, fn($e) => str_contains(mb_strtolower($e["name"]), "размер"));
+
+        return [$gNormalModifiers, $gSizesModifiers];
+
+    }
+
+    // собираем модификаторы
+    private function get_modifiers_items(array $menu, string $modifierGroupId, array $childModifiers): array {
+        // отбираем модификаторы группы
+        $items = array_filter($menu['products'], fn($e) => ($e["parentGroup"] === $modifierGroupId));
+        // пересобираем их в chefs структуру
+        $items_parsed = array_map(function($e) use($childModifiers) { 
+            // собираем параметры restrictions у модификатора
+            $modifierId = $e["itemId"];
+            $mo = array_filter($childModifiers, function($cm) use($modifierId) { return $cm["modifierId"] === $modifierId; });
+            $norestrictions = ["minQuantity" => 0, "maxQuantity" => 0, "required" => false, "byDefault" => 0, "freeQuantity" => 0];
+            $restrictions = $mo[array_key_first($mo)]["restrictions"]??$norestrictions;
+            return [
+            "modifierId" => $modifierId,
+            "modifierGroupId"=> $e["parentGroup"],
+            "name" => $e["name"],
+            "description" => $e["description"],
+            "imageUrl"=> $e["imageUrl"],
+            "portionWeightGrams"=>$e["itemSizes"][0]["weightGrams"]??0,
+            "price"=>$e["itemSizes"][0]["price"]??0,
+            "restrictions"=>$restrictions,
+            ];
+    }, $items);
+        return $this->assoc_to_array($items_parsed);
+    }
+
+    // собираем размеры товара
+    private function get_item_sizes(array $sizes, array $gSizesModifiers=null): array {
+        
+        // фишка PIZZAIOLO:
+        // если есть специальный модификатор для размеров, 
+        // то строим размерный ряд из него        
+        if($gSizesModifiers!==null && count($gSizesModifiers)>0){
+
+            $gSizesModifier = $gSizesModifiers[array_key_first($gSizesModifiers)];            
+            $mainPrice = (int) $sizes[0]["price"];
+            $measureUnitType = $sizes[0]["measureUnitType"];
+            $weightGrams = (int) $sizes[0]["weightGrams"];
+
+            $sizes_parsed = [];
+
+            $count = 0;
+            foreach($gSizesModifier["items"] as $item){             
+                // устанавливаем по умолчанию второй по счету размер; 
+                $isDefault = $count==1; $count++;                
+                $sizes_parsed[] = [
+                    "sizeCode"=>"",
+                    "sizeId" => $item["modifierId"],
+                    "sizeName" => $item["name"],
+                    "isDefault"=> $isDefault,
+                    "price" => (int) $item["price"] + $mainPrice,
+                    "originalPrice" => (int) $item["price"],
+                    "sizeGroupId"=> $item["modifierGroupId"],
+                    "measureUnitType"=> $measureUnitType,
+                    "portionWeightGrams" => (int) $item["portionWeightGrams"] + $weightGrams,
+                ];
+            }
+
+        // иначе, применяем обычные размеры
+        }else{            
+            $sizes_parsed = array_map(fn($e)=>[
+                "sizeCode"=>"",
+                "sizeId" => $e["sizeId"],
+                "sizeName" => $e["sizeName"],
+                "isDefault"=> false,
+                "price" => $e["price"],
+                "originalPrice" => null,
+                "sizeGroupId"=> "",
+                "measureUnitType"=>$e["measureUnitType"],
+                "portionWeightGrams" => $e["weightGrams"],
+            ], $sizes);
+            // устанавливаем первый по счету размер по умолчанию
+            $sizes_parsed[0]["isDefault"] = true;
+            // ------------------------------------------------------------------
+            // если названия хотябы одного размера не укзано, 
+            // то присваеваем всем размерам свои названия S, M, L, XL, XXL, XXXL;
+            // ------------------------------------------------------------------
+            // ищем пустые названия размеров            
+            if((in_array('', array_column($sizes_parsed, 'sizeName'), true))){
+                // если есть хоть одно пустое название:
+                // сортируем по цене
+                usort($sizes_parsed, function($a, $b) {
+                    return $a['price'] <=> $b['price'];
+                });
+                // Добавляем размеры
+                foreach ($sizes_parsed as $index => &$item) {
+                    if ($index == 0) {
+                        $item['sizeName'] = 'S';
+                    } elseif ($index == 1) {
+                        $item['sizeName'] = 'M';
+                    } elseif ($index == 2) {
+                        $item['sizeName'] = 'L';
+                    } else {
+                        $xCount = $index - 2; // Количество "X" = позиция - 2
+                        $item['sizeName'] = str_repeat('X', $xCount) . 'L';
+                    }
+                }
+                unset($item); // Разрываем ссылку после цикла
+            }
+        }
+
+        return $sizes_parsed;
+    }
+
+    private function assoc_to_array($assoc) {
+        $new_array = [];
+        foreach ($assoc as $key => $value) {
+            $new_array[] = $value;
+        }
+        return $new_array;
+    }
+
+}
+
+
+
+
+?>
