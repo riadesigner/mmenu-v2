2b39de08cf474a687b608abe0fe6153d5cb3fa46
riadesigner <evgenii.pogrebnyak@gmail.com>
2025-06-21 12:37:25 +1000

    ok

--- app/iiko-hook.php
+++ app/iiko-hook.php
@@ -3,7 +3,7 @@
 define("BASEPATH",__file__);
 require_once 'config.php';
 require_once WORK_DIR.APP_DIR.'core/common.php';
-require_once WORK_DIR.APP_DIR.'core/class.Iiko_hook.php';
+require_once WORK_DIR.APP_DIR.'core/class.iiko_hook.php';
 
 
 $data = json_decode(file_get_contents('php://input'), true);
