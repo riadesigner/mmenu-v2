eb65828ee3dc24ba82cda1c4d768178dcb1f4137
riadesigner <evgenii.pogrebnyak@gmail.com>
2025-06-24 21:26:10 +1000

    исправлена ошибка в файла class.iiko.order.php

---
+++ app/export/app-v2-126-stable/site/views/tpl.site.php
@@ -0,0 +1,271 @@
+<?php
+	require_once('tpl.site_header.php');
+	require_once('tpl.site_section_create.php');
+	require_once('tpl.site_description.php');
+?>
+	
+<div class="first-page-content">
+	<div class="section-why">
+		<div class="section-why-reason">
+			<img class="section-why-reason__image" src="./site/i/site-ico-01.svg">
+			<div class="section-why-reason__text font-normal-size">				
+				Легко создать. <br>Просто<br> обновлять
+			</div>
+		</div>
+		<div class="section-why-reason">
+			<img class="section-why-reason__image" src="./site/i/site-ico-02.svg">
+			<div class="section-why-reason__text font-normal-size">
+				Управляется<br> с телефона <br>Работает везде
+			</div>
+		</div>
+		<div class="section-why-reason">
+			<img class="section-why-reason__image" src="./site/i/site-ico-03.svg">
+			<div class="section-why-reason__text font-normal-size">
+				Не требует <br>установки!<br>Доступно сразу
+			</div>
+		</div>
+	</div>	
+	<div class="section-what">		
+		
+		<h2>Электронное <nobr>Меню –</nobr> <nobr>это удобно,</nobr> <br>современно <nobr>и экономично!</nobr></h2>
+		<div class="site-textblock-2">
+			Предоставьте вашим посетителям альтернативный вариант подачи Меню! 
+			Замените бумажное Меню на удобное веб-приложение! Потрясающие современные технологии сделали
+			возможным создать такое Меню в пару касаний на телефоне. Обновляйте ваше Меню без затрат на печать.			
+			 </div>
+		
+		<hr class="hr-style-1">
+		
+		<h2>Управляйте Меню с телефона. <br>Это по-настоящему просто!</h2>
+		
+		<div class="site-textblock-2">
+			Фотографируйте и публикуйте ваши лучшие блюда нажатием пары кнопок на телефоне,
+			<nobr>а также</nobr> оперативно меняйте любую информацию <nobr>о товаре.</nobr> 
+			Понятный интерфейс поможет вам быстро освоиться. 
+			А ваши посетители оценят легкость открытия меню на своих устройствах. 
+			Ведь им не придется <a href="<?=SITE::get_link('home');?>/#section-qr-code">ничего устанавливать!</a></div>			
+
+	</div>
+	
+	<div class="section-yellow">
+		<div class="section-yellow-top">
+			<div class="section-yellow-top__image">
+				<img src="./site/i/site2-exp-01-8.png">
+				<div class="section-yellow-top__anim">
+					<div class="section-yellow__anim-loader"><img src="./site/i/site2-loader-01.svg"></div>					
+				</div>
+			</div>
+			<div class="section-yellow-top__text">
+				<h2><span>Быстрое,</span> <br><span>отзывчивое,</span><br> <span>понятное</span></h2>
+				<p class="font-normal-size">Современное, функциональное меню.<br> Быстрая загрузка, плавная анимация.<br> Приятный, отзывчивый интерфейс.</p>				
+				<img class="section-yellow-top__decor-02" src="./site/i/site2-exp-29.svg">
+			</div>
+			<img class="section-yellow-top__decor-01" src="./site/i/site2-exp-25.svg">			
+		</div>
+		
+
+		<div class="section-yellow-bottom">
+			<div class="section-yellow-bottom__image">
+				<img class="section-yellow-bottom__image-desktop" src="./site/i/site2-exp-04-8.png">
+				<img class="section-yellow-bottom__image-mobile" src="./site/i/site2-exp-30-8.png">
+				<div class="section-yellow-bottom__anim">
+					<div class="section-yellow__anim-loader"><img src="./site/i/site2-loader-01.svg"></div>					
+				</div>				
+			</div>			
+			<div class="section-yellow-bottom__text">
+				<h2><span>Простая</span> <br><span>настройка</span></h2>
+				<p class="font-normal-size">Все самое главное уже настроено, <br>впишите адрес вашего кафе <nobr>и начните</nobr><br> добавлять разделы и блюда. </p>
+			</div>
+		</div>
+		<img class="section-yellow-decor-1" src="./site/i/site2-exp-02.svg">
+		<img class="section-yellow-decor-2" src="./site/i/site2-exp-03.svg">
+		<img class="section-yellow-decor-3" src="./site/i/site2-exp-31.svg">
+	</div>
+
+	<div class="section-all-devices">
+		<div class="section-all-devices__content">
+			<div class="section-all-devices__image">
+				<img src="./site/i/site2-exp-05-8.png">
+			</div>
+			<div class="section-all-devices__text">
+				<p class="font-normal-size">Создается <a href="<?=SITE::get_link('help_quick_creating');?>">БЫСТРО</a></p>
+				<p class="font-normal-size">Управляется <a href="<?=SITE::get_link('help_edit_items');?>">ПРОСТО</a></p>
+				<p class="font-normal-size">Адаптируется под экраны разных размеров для комфортного просмотра</p>
+				<p class="font-normal-size">Меню имеет <a href="<?=SITE::get_link('help_link_to_menu');?>">КРАСИВЫЙ АДРЕС</a> в&nbsp;интернете</p>
+			</div>	
+		</div>
+		<img class="section-all-devices__decor" src="./site/i/site2-exp-06.svg">
+		<img class="section-all-devices__decor-2" src="./site/i/site2-exp-08.svg">	
+		<img class="section-all-devices__decor-3" src="./site/i/site2-exp-09.svg">	
+		<img class="section-all-devices__decor-4" src="./site/i/site2-exp-26.svg">
+	</div>
+
+	<div class="section-other-features">
+		<div class="section-other-features__content">
+		<div class="section-other-features__block">
+			<div class="section-other-features__block-image"></div>
+			<h2><span>Создать</span><br> <span>за десять</span><br> <span>секунд?</span></h2>
+			<h3><span>Создать за десять</span><br><span>секунд?</span></h3>
+			<p class="font-normal-size">Да! Вы указываете ваш электронный 
+				адрес и название кафе, сервис создает для вас аккаунт, уникальный адрес и высылает вам
+				доступ в <a href="<?=SITE::get_link('help');?>#link-control-panel">Панель Управления</a> вашим меню. 
+			</p>
+		</div>		
+		<div class="section-other-features__block">
+			<div class="section-other-features__block-image"></div>
+			<h2><span>В кадре</span> <br><span>только</span><br> <span>лучшее!</span></h2>
+			<h3><span>В кадре только</span><br><span>лучшее!</span></h3>
+			<p class="font-normal-size">У шеф-повара вдохновение!?
+			Лучшие кулинарные произведения ваш <a href="<?=SITE::get_link('help_edit_items');?>">повар сможет</a> мгновенно 
+			опубликовать в&nbsp;меню. Ведь телефон всегда под рукой.</p>
+		</div>
+		<div class="section-other-features__block">
+			<div class="section-other-features__block-image"></div>
+			<h2><span>В вашем</span><br> <span>кафе есть</span><br> <span>доставка?</span></h2>
+			<h3><span>В вашем кафе есть</span><br><span>доставка?</span></h3>
+			<p class="font-normal-size">Окей, все готово для приема заказов. В&nbsp;меню встроена корзина покупок. 
+				Заказы отправляются в <a href='<?=SITE::get_link('help');?>/#cart-customizing'>Телеграм</a> и на электронную почту.				
+			</p>
+		</div>
+		</div>
+	</div>
+	<div class="use-subdomain">		
+		<div class="use-subdomain__text">					
+			<h2>Адрес меню в&nbsp;интернете</h2>
+			<p class="font-normal-size">У вашего меню будет свой уникальный адрес в сети интернет. 
+				<a href="<?=SITE::get_link('help_link_to_menu');?>">Настройте его</a> на&nbsp;свой вкус</p>
+		</div>
+		<div class="use-subdomain__image">
+			<img class="use-subdomain__decor-02-desktop" src="./site/i/site2-exp-12-8.png">
+			<img class="use-subdomain__decor-02-tablet" src="./site/i/site2-exp-27-8.png">
+			<img class="use-subdomain__decor-02-phone" src="./site/i/site2-exp-33-8.png">
+			<div class="use-subdomain__image-title">Используйте <span>название <br>вашего кафе</span> в адресе Меню </div>
+		</div>
+		<img class="use-subdomain__decor-01-desktop" src="./site/i/site2-exp-11.svg">
+		<img class="use-subdomain__decor-01-phone" src="./site/i/site2-exp-42-8.png">
+		
+	</div>
+	
+	<div class="section-qr-code" id="section-qr-code">
+		<div class="section-qr-code__content">
+			<div class="section-qr-code__image">
+				<img src="./site/i/site2-exp-15-8.png">
+			</div>			
+			<div class="section-qr-code__text">
+				<h2><span>Не требует установки!</span><br><span>Работает сразу</span></h2>
+				<p class="font-normal-size">Вашим посетителям ничего не нужно устанавливать. 
+					Достаточно перейти <strong>по&nbsp;ссылке</strong> или
+					отсканировать <strong><nobr>QR-код.</nobr></strong> 
+					Откроется самая свежая версия вашего&nbsp;меню.</p>
+				<div class="section-qr-code__text-more"><a href="<?=SITE::get_link('easy_open');?>"><span>Подробнее</span></a></div>
+			</div>
+		</div>
+		<img class="section-qr-code__decor-01-desktop" src="./site/i/site2-exp-17.svg">
+		<img class="section-qr-code__decor-01-phone" src="./site/i/site2-exp-34-8.png">
+	</div>
+
+	<div class="section-samples">
+		<div class="section-samples__content">
+			<div class="section-samples__text">
+				<h2><span>Примеры:</span></h2>
+				<div class="section-samples__text-message">
+				<p class="font-normal-size">
+					Посмотрите примеры меню. Мы предлагаем вам 
+					несколько стилей оформления.
+				</p>
+				<p class="font-normal-size">
+					Создайте свое меню! А если возникнут вопросы&nbsp;– 
+					мы <a href="<?=SITE::get_link('contacts');?>">всегда&nbsp;рядом</a> и&nbsp;готовы помочь.					
+				</p>
+				</div>
+			</div>
+			<div class="section-samples__links">
+				<?php
+
+					if(count($CFG->cafe_sample_links)){
+						echo "<ul>";
+						foreach ($CFG->cafe_sample_links as $cafe) {
+							$cafe_name = $cafe['cafe-name'];
+							$cafe_uniq = $cafe['cafe-uniq'];
+							$cafe_subdomain = $cafe['subdomain'];
+							// OPEN IN SELF							
+							$link = implode("\n",[
+									'<a href="'.SITE::get_link('home').'/#chefsmenu'.$cafe_uniq.'" ',
+									'class="chefsmenu-link" data-cafe="'.$cafe_uniq.'">'
+								]); 
+
+							echo implode("\n", [
+								'<li>',
+								$link,
+								'<div><h2>Меню</h2><p>'.$cafe_name.'</p></div>',
+								'</a>',
+								'</li>'
+							]);
+						}
+						echo "</ul>";
+					}
+				?>
+
+			</div>
+		</div>	
+		<img class="section-samples__decor-01" src="./site/i/site2-exp-16.svg">
+	</div>
+
+	<div class="section-bottom-divider">		
+		<img class="section-bottom-divider__phone" src="./site/i/site2-exp-36.svg">
+		<img class="section-bottom-divider__tablet" src="./site/i/site2-exp-28.svg">
+		<img class="section-bottom-divider__desktop" src="./site/i/site2-exp-20.svg"> 
+	</div>
+
+</div>
+
+<script type="text/javascript" src="./site/dist/lottie_svg.min.js"></script>
+
+<script>
+	
+	var anim1wrapper = $('.section-yellow-top__anim')[0];	
+    var animData1 = {
+        wrapper: anim1wrapper,
+        animType: 'svg',
+        loop: true,
+        prerender: true,
+        autoplay: true,
+        path: './site/i/data-anim-1.json'
+
+    };
+   
+	var anim2wrapper = $('.section-yellow-bottom__anim')[0];	
+    var animData2 = {
+        wrapper: anim2wrapper,
+        animType: 'svg',
+        loop: true,
+        prerender: true,
+        autoplay: true,
+        path: './site/i/data-anim-2.json'
+
+    };
+
+    $(function() {
+
+	    var anim1 = bodymovin.loadAnimation(animData1);
+	    var anim2 = bodymovin.loadAnimation(animData2);    
+
+	    anim1.addEventListener('data_ready',function(){	        
+	        setTimeout(function(){
+	        	$('.section-yellow-top__anim .section-yellow__anim-loader').addClass('hidden');
+	        },300);
+	    });
+	    anim2.addEventListener('data_ready',function(){	        
+	        setTimeout(function(){
+	        	$('.section-yellow-bottom__anim .section-yellow__anim-loader').addClass('hidden');
+	        },300);
+	    });   
+    	
+    });
+
+
+</script>
+
+<?php
+	require_once('tpl.site_footer.php');
+?>
