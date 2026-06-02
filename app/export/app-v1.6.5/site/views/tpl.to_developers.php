00b86c50ee16f2a5069b4d9e882eee4c6ecb391d
evgenii <evgenii@MacBook-Pro-evgenii.local>
2026-06-03 08:43:11 +1000

    ver 1-6-5

---
+++ app/export/app-v1.6.5/site/views/tpl.to_developers.php
@@ -0,0 +1,20 @@
+<?php
+	require_once('tpl.site_header.php');
+?>
+
+<div class="page-main-content">
+	<div class="page-breadcrumbs">
+<a href="<?=SITE::get_link('home');?>"><?=LNG::get("lng_main");?></a> / <span><?=LNG::get("lng_to_developers");?></span>
+	</div>
+	<div class="page-article">
+		<h1>Разработчикам</h1>
+		<div class="page-article__section">
+			<h2>Партнерская программа</h2>			
+			<p>...</p>
+		</div>
+	</div>
+</div>
+
+<?php
+	require_once('tpl.site_footer.php');
+?>
